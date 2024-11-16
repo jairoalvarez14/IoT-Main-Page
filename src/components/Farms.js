b2e451aade428fbc812fca011371b9074a8260e5
Hector <hectortorres7419@gmail.com>
2024-11-16 10:30:22 -0600

    TODO on popup, have to assing each farm with their popup

--- src/components/Farms.js
+++ src/components/Farms.js
@@ -1,6 +1,6 @@
 // Sample farm data with image URLs
 const farms = [
-    { id: "1", name: "Farm 1", link: "", img: "'https://www.securities.io/wp-content/uploads/2023/11/Aeroponics.png'" },
+    { id: "1", name: "Farm 1", link: "https://ioturbanfarm-1.onrender.com", img: "'https://www.securities.io/wp-content/uploads/2023/11/Aeroponics.png'" },
     { id: "2", name: "Farm 2", link: "", img: "'https://agrotonomy.com/wp-content/uploads/2018/03/aeroponic-tower-farm.jpg'" },
   ];
 
--- src/components/PopupFarm.jsx
+++ src/components/PopupFarm.jsx
@@ -4,11 +4,15 @@ const PopupFarm = () => {
     document.getElementById("popup-1").classList.remove("active");
   };
 
+  const openFarm = () => {
+    window.open('https://ioturbanfarm-1.onrender.com')
+  }
+
   return (
     <>
       <div className="popup" id="popup-1">
-        <div className="overlay absolute top-0 left-0 w-full h-full bg-black/70 z-[1] hidden"></div>
-        <div className="content absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] scale-0 bg-[#f9f7f3] w-[450px] h-[350px] overflow-scroll z-[2] text-center p-5 box-border">
+        <div className="overlay absolute top-0 left-0 w-full h-full bg-black/70 z-[1] opacity-0 pointer-events-none  backdrop-blur-sm"></div>
+        <div className="content absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] scale-0 bg-[#f9f7f3] w-[60vw] h-[50vh] overflow-auto z-[2] text-center p-5 box-border opacity-0 pointer-events-none rounded-xl ">
           <div
             className="absolute right-5 top-5 w-7 h-7 text-[25px] font-semibold leading-7 text-center rounded-[50%] cursor-pointer"
             onClick={removeClass}
@@ -27,6 +31,7 @@ const PopupFarm = () => {
             Suspendisse luctus volutpat lorem. Nunc quam nisl, mollis nec
             vulputate eu, malesuada sed magna.
           </p>
+          <a onClick={openFarm} className="cursor-pointer hover:underline hover:text-blue-500 transition-colors duration-300 ease-in-out active:text-purple-600">Farm 1</a>
         </div>
       </div>
     </>
