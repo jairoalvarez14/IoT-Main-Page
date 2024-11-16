5b6d8eb6065f96645889a976cfb5f7d5e34165b2
jairoalvarez14 <giovannial2000@gmail.com>
2024-11-16 11:05:10 -0600

    before backend

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import InfoHowPlantsGrow from "./components/InfoHowPlantsGrow";
 
 const MainPage = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/pages/how-plants-grow-info"
+          element={<InfoHowPlantsGrow />}
+        />
       </Routes>
     </div>
   );
